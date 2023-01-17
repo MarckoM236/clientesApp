be82309bc97917eef02e8abde109116fd12bc9b8
Marco Marin <marcko236@gmail.com>
2023-01-17 17:02:13 -0500

    creardo proyecto y configurando .env

---
+++ app/Models/ClienteModel.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ClienteModel extends Model{
+    protected $table = 'cliente';
+    protected $primaryKey = 'id';
+
+    protected $returnType = 'array';
+    protected $allowedFiles = ['nombre','apellido','telefono','correo']; //campos a insertar en la DB
+
+    protected $useTimestamps= true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+
+    //Validaciones
+    protected $validationRules = [
+        'nombre' => 'required|alpha_space|min_length[3]|max_length[75]',
+        'apellido' => 'required|alpha_space|min_length[3]|max_length[75]',
+        'telefono' => 'required|alpha_number_space|min_length[8]|max_length[8]',
+        'correo' => 'permit_empty|valid_email|max_length[85]'
+    ];
+
+    //Mensajes personalizados para las reglas
+    protected $validationMessage = [
+        'correo' =>[
+                        'valid_email' => 'Estimado usuario, debe ingresar un email valido'
+        ]
+        ];
+
+}
